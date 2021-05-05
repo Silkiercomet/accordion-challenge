a443f143c9fbb993d69fe32c15e5348e19a68767
luis Colina <79709581+Silkiercomet@users.noreply.github.com>
2021-05-05 13:30:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-for now
+let drop = document.querySelectorAll(".toggled");
+
+let g = document.getElementById('my_div');
+for (let i = 0, len = g.children.length; i < len; i++){
+
+    (function(index){
+        g.children[i].onclick = function(){
+              drop[i].classList.toggle("show")
+        }    
+    })(i);
+
+}
